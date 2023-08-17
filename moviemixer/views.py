b0efd1b7262ed23237fe8c9de6bcf04a4bdbc0d6
root <tyler.stamm1313@gmail.com>
2023-08-17 15:13:41 +0000

    Updated views

--- moviemixer/views.py
+++ moviemixer/views.py
@@ -11,40 +11,119 @@ from rest_framework import status
 import requests
 from .models import movie
 
-def get_recommendations(answers):
-    genre = answers.get("genre")
-    decade = answers.get("decade")
-    actors = answers.get("actors")
-    directors = answers.get("directors")
-    duration = answers.get("duration")
-    mood = answers.get("mood")
-    rating = answers.get("rating")
+def get_all_movies(request):
+    url = "https://moviesdatabase.p.rapidapi.com/titles"
+    headers = {
+	    "X-RapidAPI-Key": "ede169c197msh94c9ec518d4a5e2p18d147jsn10e88d3b6b4b",
+	    "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com"
+}
 
-    # Build a query based on the selected answers
-    query = movie.objects.all()  # Replace 'Movie' with your actual model name
-    if genre:
-        query = query.filter(genre=genre)
-    if decade:
-        query = query.filter(decade=decade)
-    if actors:
-        query = query.filter(actors__icontains=actors)
-    if directors:
-        query = query.filter(directors__icontains=directors)
-    if duration:
-        query = query.filter(duration=duration)
-    if mood:
-        query = query.filter(mood=mood)
-    if rating:
-        query = query.filter(rating=rating)
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return JsonResponse(response.json(), safe=False)
+    else:
+        return JsonResponse({"error": "Failed to fetch movies"}, status=500)
+    
+import requests
 
-    # Return recommended movies as a list of dictionaries
-    recommended_movies = [{"title": movie.title} for movie in query]
-    return recommended_movies
+def filter_movies(request):
+    genre = request.GET.get("genre")
+    title_type = request.GET.get("titleType")
+    start_year = request.GET.get("startYear")
+    end_year = request.GET.get("endYear")
 
-def get_movie_recommendations(request):
-    recommended_movies = get_recommendations(request.query_params)
+    # Fetch movie data from the API
+    url = "https://moviesdatabase.p.rapidapi.com/titles"
+    headers = {
+        'X-RapidAPI-Key': 'ede169c197msh94c9ec518d4a5e2p18d147jsn10e88d3b6b4b',
+        "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        return JsonResponse({"error": "Failed to fetch movies from API"}, status=500)
+    
+    movies_data = response.json()
 
-    return JsonResponse(recommended_movies, safe=False)
+    # Apply filtering to the fetched data
+    filtered_movies = []
+    for movie_data in movies_data["results"]:
+        if (
+            (not genre or genre in movie_data["genres"])
+            and (not title_type or title_type == movie_data["titleType"])
+            and (not start_year or int(start_year) <= int(movie_data["releaseYear"]))
+            and (not end_year or int(end_year) >= int(movie_data["releaseYear"]))
+        ):
+            filtered_movies.append({"title": movie_data["titleText"]})
+
+    return JsonResponse(filtered_movies, safe=False)
+
+# def filter_movies(request):
+#     genre = request.GET.get("genre")
+#     title_type = request.GET.get("titleType")
+#     start_year = request.GET.get("startYear")
+#     end_year = request.GET.get("endYear")
+
+#     query = movie.objects.all()
+
+#     if genre:
+#         query = query.filter(genre=genre)
+#     if title_type:
+#         query = query.filter(title_type=title_type)
+#     if start_year:
+#         query = query.filter(release_year__gte=start_year)
+#     if end_year:
+#         query = query.filter(release_year__lte=end_year)
+
+#     filtered_movies = [{"title": movie.title} for movie in query]
+#     return JsonResponse(filtered_movies, safe=False)
+
+    # genre = request.GET.get("genre")
+
+
+    # query = movie.objects.all()
+    # if genre:
+    #     query = query.filter(genre=genre)
+
+    # filtered_movies = [{"title": movie.title} for movie in query]
+    # return JsonResponse(filtered_movies, safe=False)
+
+
+# def get_recommendations(answers):
+#     try:
+#         genre = answers["genre"]
+#         decade = answers["decade"]
+#         actors = answers["actors"]
+#         directors = answers["directors"]
+#         duration = answers["duration"]
+#         mood = answers["mood"]
+#         rating = answers["rating"]
+#     except KeyError:
+#         pass
+
+#     query = movie.objects.all()  
+#     if genre:
+#         query = query.filter(genre=genre)
+#     if decade:
+#         query = query.filter(decade=decade)
+#     if actors:
+#         query = query.filter(actors__icontains=actors)
+#     if directors:
+#         query = query.filter(directors__icontains=directors)
+#     if duration:
+#         query = query.filter(duration=duration)
+#     if mood:
+#         query = query.filter(mood=mood)
+#     if rating:
+#         query = query.filter(rating=rating)
+
+#     # Return recommended movies as a list of dictionaries
+#     recommended_movies = [{"title": movie.title} for movie in query]
+#     return recommended_movies
+
+# def get_movie_recommendations(request):
+#     recommended_movies = get_recommendations(request.query_params)
+#     return JsonResponse(recommended_movies, safe=False)
+
 
 
 
